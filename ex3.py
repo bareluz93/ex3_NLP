53bad084084a012e14fa700bf00254d8bddad42a
bar eluz <bareluz93@gmail.com>
2018-01-01 14:19:31 +0200

    no copy

--- ex3.py
+++ ex3.py
@@ -17,9 +17,11 @@ tagged_sents_orig=dependency_treebank.tagged_sents()
 tagged_sents = []
 for sent in tagged_sents_orig:
     tagged_sents.append([('ROOT', 'ROOT')] + sent)
-train_sents=tagged_sents[:int(len(parsed_sents)*0.9)]
-train=parsed_sents[:int(len(parsed_sents)*0.9)]
-test=parsed_sents[int(len(parsed_sents)*0.9):]
+
+train_tagged= tagged_sents[:int(len(parsed_sents) * 0.1)]
+train_parsed= parsed_sents[:int(len(parsed_sents) * 0.1)]
+test_parsed= parsed_sents[int(len(parsed_sents) * 0.1):int(len(tagged_sents) * 0.12)]
+test_tagged= tagged_sents[int(len(tagged_sents) * 0.1):int(len(tagged_sents) * 0.12)]
 def get_all_possible_tags_and_words(data_set):
     all_words = set()
     all_tags=set()
@@ -35,7 +37,8 @@ def get_all_possible_tags_and_words(data_set):
     all_words.sort()
     return all_words,all_tags
 
-all_words,all_tags = get_all_possible_tags_and_words(train_sents)
+# all_words,all_tags = get_all_possible_tags_and_words(train_sents)
+all_words,all_tags = get_all_possible_tags_and_words(tagged_sents)
 tag2i = {pos: i for (i, pos) in enumerate(all_tags)}
 word2i = {word: i for (i, word) in enumerate(all_words)}
 N = len(all_words)
@@ -51,12 +54,15 @@ def tag_bigram_feature(w1,w2):
     # feature_vec[tag2i[w1] * T+tag2i[w2]]=1
     return tag2i[w1] * T+tag2i[w2]
 
-#
+# def distance_feature():
+
 def feature_function(w1, t1, w2, t2):
     w_feature = word_bigram_feature(w1,w2)
     t_feature = tag_bigram_feature(t1,t2)
-    # vstack([w_feature, t_feature])
-    return sparse_vector([w_feature,t_feature])
+    temp1=sparse_vector([w_feature],N**2)
+    temp2=sparse_vector([t_feature],T**2)
+    temp1.concatenate(temp2)
+    return temp1
 
 
 # the feature_function gets 2 words and return feature-vector; the weight is this vector multiply w;
@@ -68,16 +74,16 @@ def sentence_to_full_graph(feature_function, w, sentence):
         graph[i] = dict()
         for j in range(len(sentence)):
             if i!=j:
-                weight = feature_function(sentence[i][0],sentence[i][1], sentence[j][0], sentence[j][1]).sparse_dot(w) # todo tags
+                weight = feature_function(sentence[i][0],sentence[i][1], sentence[j][0], sentence[j][1]).sparse_dot_by_sparse(w)
                 graph[i][j] = -weight
     return graph
-def sum_tree(tree,parsed_sent):
-    ret=sparse_vector([])
+def sum_tree(tree, tagged_sent):
+    ret=sparse_vector([],N**2+T++2)
     for node1 in tree:
         neighbours=tree[node1]
         for node2 in neighbours:
-            feature_vec=feature_function(parsed_sent[node1][0], parsed_sent[node1][1], parsed_sent[node2][0],parsed_sent[node2][1])
-            ret=ret.add(feature_vec)
+            feature_vec=feature_function(tagged_sent[node1][0], tagged_sent[node1][1], tagged_sent[node2][0], tagged_sent[node2][1])
+            ret.add(feature_vec)
     return ret
 
 # w_z = np.zeros((N**2 + T**2,1))
@@ -98,26 +104,55 @@ def to_tree(prs_sent):
             ret[w1][w2] = 0
     return ret
 
-t=to_tree(parsed_sents[0])
-sum=sum_tree(t,train_sents[0])
-print(t)
-print("...........................")
-print(train_sents[0])
-print(".............................")
-print(sum.vec)
+# t=to_tree(parsed_sents[0])
+# sum=sum_tree(t,train_tagged[0])
+# print(t)
+# print("...........................")
+# print(train_tagged[0])
+# print(".............................")
+# print(sum.vec)
 
 def perceptron(learning_rate=1,itertations=2):
-    weight=np.zeros(N**2+T++2)
-    rand_iter = list(range(len(train)))
+    # weight=np.zeros(N**2+T++2)
+    weight=sparse_vector([],N**2+T++2)
+    rand_iter = list(range(len(train_parsed)))
     random.shuffle(rand_iter)
 
+    w_sum = sparse_vector([],N**2+T++2)
     for i in range(itertations):
         for j in rand_iter:
-            G = sentence_to_full_graph(feature_function, weight, train[j])
+            G = sentence_to_full_graph(feature_function, weight, train_tagged[j])
             T_opt=mst.mst(0,G)
+            t = to_tree(train_parsed[j])
+            temp=sum_tree(t, train_tagged[j])
+            temp.sub(sum_tree(T_opt, train_tagged[j]))
+            temp.mult_by_scalar(learning_rate)
+            weight.add(temp)
+            w_sum.add(weight)
+            w_sum.mult_by_scalar(1.0 / (len(train_tagged) * itertations))
+    return w_sum
+    # return weight
 
-            weight=weight+learning_rate*()
+def score_sent(w_train,tagged_sent,feature_function,gold_tree):
+    G = sentence_to_full_graph(feature_function, w_train, tagged_sent)
+    T = mst.mst(0, G)
+    gold_tree=to_tree(gold_tree)
+    num_of_right_edges=0
+    for node in gold_tree:
+        if node in T.keys():
+            neighbours=gold_tree[node].keys()
+            for node2 in neighbours:
+                if node2 in T[node].keys():
+                    num_of_right_edges+=1
+    return num_of_right_edges/len(tagged_sent)
+
+def score(w_train, feature_function, test_gold, test_tag):
+    sum_of_scores = 0
+    for i in range(len(test_gold)):
+        sum_of_scores += score_sent(w_train,test_tag[i],feature_function,test_gold[i])
+    return sum_of_scores / len(test_gold)
 
 
-
-
+w = perceptron()
+print(score(w,feature_function, train_parsed, train_tagged))
+# print(score(w,feature_function, test_parsed, test_tagged))
--- sparse_vector.py
+++ sparse_vector.py
@@ -1,25 +1,47 @@
 import numpy as np
 import copy
+
+
 class sparse_vector():
-    def __init__(self,indexes):
-        self.vec ={}
+    def __init__(self, indexes, size):
+        self.vec = {}
+        self.size = size
         for idx in indexes:
-            self.vec[idx]=1
+            self.vec[idx] = 1
+
+    def concatenate(self, other):
+        for node in other.vec:
+            self.vec[node + self.size] = other.vec[node]
+        self.size += other.size
+
+    def sparse_dot_by_sparse(self, other):
+        sum = 0
+        for key in self.vec:
+            if key in other.vec:
+                sum += self.vec[key] * other.vec[key]
+        return sum
 
     def sparse_dot(self, weight_vec):
-        return np.sum(weight_vec[list(self.vec.keys())])
-    def add(self,other):
-        ret = sparse_vector([])
-        ret.vec = copy.deepcopy(self.vec)
+        sum = 0
+        for key in self.vec:
+            sum += self.vec[key] * weight_vec[key]
+        return sum
+
+    def mult_by_scalar(self, a):
+        for key in self.vec:
+            self.vec[key] *= a
+
+    def add(self, other):
+        # ret = sparse_vector([],self.size)
+        # ret.vec = copy.deepcopy(self.vec)
         for key in other.vec:
-            if key in ret.vec:
-                ret.vec[key] += other.vec[key]
+            if key in self.vec:
+                self.vec[key] += other.vec[key]
             else:
-                ret.vec[key] = other.vec[key]
-        return ret
-    def sub(self,other):
-        other_copy = sparse_vector([])
-        other_copy.vec = copy.deepcopy(self.vec)
-        for key in other_copy.vec:
-            other_copy.vec[key]=other.vec[key]*(-1)
-        return self.add(other)
+                self.vec[key] = other.vec[key]
+
+    def sub(self, other):
+        # other_copy = sparse_vector([],self.size)
+        # other_copy.vec = copy.deepcopy(other.vec)
+        other.mult_by_scalar(-1)
+        self.add(other)
